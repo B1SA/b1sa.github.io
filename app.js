a0893d9a1a7d5ad14f648c00e2c047d06c66bfe5
Ralph Oliveira <ralphive@protonmail.com>
2020-12-11 11:09:34 +0000

    expose only public repos

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const { Octokit } = require("@octokit/rest");
 
 const octokit = new Octokit({
-  auth: process.env.GITHUB_TOKEN,
+    auth: process.env.GITHUB_TOKEN,
 });
 
 octokit.teams.listReposInOrg({
     org: process.env.GITHUB_ORG,
     team_slug: process.env.GITHUB_TEAM,
-  })
-  .then(({ data }) => {
-     console.dir(data)
-     console.log("Done!")
-  });
+}).then(({ data }) => {  
+    var publicRepos = []
+    data.forEach(repo => {
+        if(!repo.private){
+            item = {id: repo.id,
+                    name: repo.name,
+                    description: repo.description,
+                    forks_count: repo.forks_count,
+                    html_url: repo.html_url,
+                    homepage: repo.homepage,
+                    language: repo.language,
+                    stargazers_count: repo.stargazers_count,
+                    watchers_count: repo.watchers_count}
+
+            publicRepos.push(item)
+        }
+    });
+    console.log(publicRepos)
+});
