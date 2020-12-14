e7c3487bcece055404d345529c45b1b8ab029d45
Ralph Oliveira <ralphive@protonmail.com>
2020-12-14 19:28:59 +0000

    add members

--- ui.js
+++ ui.js
@@ -2,6 +2,9 @@ $.getJSON("./data/data.json", function(data) {
     addRepos(data.repos)
 });
 
+addRepos(data.repos)
+addMembers(data.members)
+
 function addRepos(repos){
     
     repos.forEach(repo => {
@@ -20,3 +23,15 @@ function addRepos(repos){
         
     });
 };
+
+function addMembers(members){
+    
+    members.forEach(member => {
+        var membersHtml= $('<a class="member-avatar" href="'+member.html_url+'">'
+                        +'<img src="'+member.avatar_url+'" title="'+member.login+'" width="48" height="48"></img></a>')
+        membersHtml.appendTo('#membersContent')
+
+    })
+
+
+}
