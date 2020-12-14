57b6186748652e43edbe70a9588f854a0af6df93
Ralph Oliveira <ralphive@protonmail.com>
2020-12-14 13:22:58 +0000

    add repository cards

--- ui.js
+++ ui.js
@@ -1,3 +1,22 @@
 $.getJSON("./data/data.json", function(json) {
-    console.log(json); // this will show the info it in firebug console
+    addRepos(JSON.repos)
 });
+
+function addRepos(repos){
+    
+    repos.forEach(repo => {
+        var myCol = $('<div class="col-sm-3 col-md-4 pb-2"></div>');
+        var myPanel = $('<div class="card">'
+                        +   '<div class="card-block">'
+                        +       '<div class="card-header">'
+                        +           '<span>'+repo.name+'</span>'
+                        +       '</div>'
+                        +      '<p>'+repo.description+'</p><img src="//placehold.it/50/eeeeee" class="rounded rounded-circle">'
+                        +   '</div>'
+                        +'</div>');
+
+        myPanel.appendTo(myCol);
+        myCol.appendTo('#contentPanel');
+        
+    });
+};
